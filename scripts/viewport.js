159d1514f87083cedf2242cce38f6a8243b1028f
alexmiretski <miretski.alex@gmail.com>
2025-07-23 18:57:27 -0400

    Delete scripts directory

--- scripts/viewport.js
+++
@@ -1,7 +0,0 @@
-function updateViewportHeight() {
-  const vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
-
-updateViewportHeight();
-window.addEventListener('resize', updateViewportHeight);
